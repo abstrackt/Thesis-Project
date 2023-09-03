d6dab5d9757941151cc7a7f8fc3635cc1eff7fb1
Andrzej Krupka <a.qrupka@gmail.com>
2023-09-03 17:35:54 +0200

    Added optional image flip flags to fix issues with mirrored scans

--- Scripts/meshing.py
+++ Scripts/meshing.py
@@ -6,7 +6,7 @@ from vmtk import vmtkscripts
 import numpy as np
 from stl import mesh as stmesh
  
-def preprocess(folder_name, classes):
+def preprocess(folder_name, classes, fx, fy, fz):
     if not os.path.exists(folder_name) :
         os.makedirs(folder_name)
  
@@ -54,6 +54,8 @@ def preprocess(folder_name, classes):
         minmax_filter = sitk.MinimumMaximumImageFilter()
         minmax_filter.Execute(img)
 
+        img = sitk.Flip(img, [fx, fy, fz])
+
         directory = os.path.join(in_path, "mhd")
         path = os.path.join(directory, "image.mhd")
         
@@ -90,13 +92,18 @@ def main():
     parser.add_option('-i', '--input', action='store', dest='input')
     parser.add_option('-o', '--output', action='store', dest='output', default='./')
     parser.add_option('-c', '--class', action='append', dest='seg_class')
+    parser.add_option('-x', '--flipx', action='store_true', default=False, dest='fx')
+    parser.add_option('-y', '--flipy', action='store_true', default=False, dest='fy')
+    parser.add_option('-z', '--flipz', action='store_true', default=False, dest='fz')
     (options, args) = parser.parse_args()
-    
+
     if (len(args) != 0 or options.input == '' or len(options.seg_class) == 0):
         print("Usage: python meshing.py -i <input_path> -o <output_path> (default ./) -c <class_1> -c <class_2>...")
         return
-    
-    preprocess(options.input, options.seg_class) 
+   
+    print(options.fx)
+
+    preprocess(options.input, options.seg_class, options.fx, options.fy, options.fz) 
     
     print("Preprocessing finished, now exporting mesh");
     
