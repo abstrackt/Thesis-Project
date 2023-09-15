2436b301018fa1a502068470a5db1d4aa0d2ad0f
Andrzej Krupka <a.qrupka@gmail.com>
2023-09-15 21:10:28 +0200

    Meshing script changes

--- Scripts/3d_meshing.py
+++ Scripts/3d_meshing.py
@@ -11,36 +11,26 @@ def generate_3d_mesh(in_path):
     gmsh.model.mesh.classifySurfaces(math.pi, True, True)
     gmsh.model.mesh.createGeometry()
 
-    # make extrusions only return "top" surfaces and volumes, not lateral surfaces
     gmsh.option.setNumber('Geometry.ExtrudeReturnLateralEntities', 0)
 
-    # extrude a boundary layer of 4 elements using mesh normals (thickness = 0.5)
     gmsh.model.geo.extrudeBoundaryLayer(gmsh.model.getEntities(2), [4], [1.0], True)
 
-    # extrude a second boundary layer in the opposite direction (note the `second ==
-    # True' argument to distinguish it from the first one)
-    e = gmsh.model.geo.extrudeBoundaryLayer(gmsh.model.getEntities(2), [4], [-1.0],
-                                            True, True)
+    e = gmsh.model.geo.extrudeBoundaryLayer(gmsh.model.getEntities(2), [4], [-1.0], True, True)
 
-    # get "top" surfaces created by extrusion
     top_ent = [s for s in e if s[0] == 2]
     top_surf = [s[1] for s in top_ent]
 
-    # get boundary of top surfaces, i.e. boundaries of holes
     gmsh.model.geo.synchronize()
     bnd_ent = gmsh.model.getBoundary(top_ent)
     bnd_curv = [c[1] for c in bnd_ent]
 
-    # create plane surfaces filling the holes
     loops = gmsh.model.geo.addCurveLoops(bnd_curv)
     for l in loops:
         top_surf.append(gmsh.model.geo.addPlaneSurface([l]))
 
-    # create the inner volume
     gmsh.model.geo.addVolume([gmsh.model.geo.addSurfaceLoop(top_surf)])
     gmsh.model.geo.synchronize()
 
-    # use MeshAdapt for the resulting not-so-smooth parametrizations
     gmsh.option.setNumber('Mesh.Algorithm', 1)
     gmsh.option.setNumber('Mesh.MeshSizeFactor', 0.1)
 
